d2967dfde8b424804af657ef8d3737ccad515b65
Bora <b0rik556@gmail.com>
2022-07-10 15:28:29 +0300

    add contact and nav modules

---
+++ src/contact.js
@@ -0,0 +1,33 @@
+export function loadContact() {
+    const main = document.querySelector('main');
+    main.innerHTML = '';
+    main.appendChild(makeContact());
+}
+
+function makeContact() {
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const heading = document.createElement('h2');
+    heading.textContent = 'Tell Us What You Think';
+    heading.classList.add('heading-contact');
+
+    const phoneNumber = document.createElement('h3');
+    phoneNumber.textContent = '+000-000000000';
+    phoneNumber.classList.add('contact-item');
+
+    const address = document.createElement('h3');
+    address.textContent = '69 This st. , That Country, ABC DEF.';
+    address.classList.add('contact-item');
+
+    const email = document.createElement('h3');
+    email.textContent = 'email@email.com';
+    email.classList.add('contact-item');
+
+    container.appendChild(heading);
+    container.appendChild(phoneNumber);
+    container.appendChild(address);
+    container.appendChild(email);
+
+    return container;
+}
---
+++ src/nav.js
@@ -0,0 +1,17 @@
+import makeLogo from './logo';
+import makeButton from './button';
+import loadHome from './home';
+import loadMenu from './menu';
+import loadContact from './contact';
+
+
+export function makeNav() {
+    const nav = document.createElement('nav');
+    
+    nav.appendChild(makeLogo());
+    nav.appendChild(makeButton('Home', loadHome));
+    nav.appendChild(makeButton('Menu', loadMenu));
+    nav.appendChild(makeButton('Contact', loadContact));
+
+    return nav;
+}
