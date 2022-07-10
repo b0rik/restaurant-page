6df8449e23f07a42444bae13653b8ec40384b95b
Bora <b0rik556@gmail.com>
2022-07-10 14:45:55 +0300

    add home module

---
+++ src/home.js
@@ -0,0 +1,27 @@
+export function loadHome() {
+    const main = document.querySelector('main');
+    main.innerHTML = '';
+    main.appendChild(makeHome());
+}
+
+function makeHome() {
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const heading = document.createElement('h2');
+    heading.textContent = 'Gold Black';
+    heading.classList.add('heading');
+
+    const image = document.createElement('img');
+    img.src = './images/coffee.jpg';
+    img.alt = 'coffee image';
+
+    const paragraph = document.createElement('p');
+    p.textContent = 'A cup of coffee lasts only a moment, but it is that moment that makes your day better.';
+
+    container.appendChild(heading);
+    container.appendChild(image);
+    container.appendChild(paragraph);
+
+    return container;
+}
