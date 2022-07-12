5e9aa92d97545e0fc5ba4aa3456e8a678d99ee01
Bora <b0rik556@gmail.com>
2022-07-12 13:28:11 +0300

    fix navbar

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  watch: true,
   mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
-    ],
   },
 };
